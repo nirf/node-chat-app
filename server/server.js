9d20664a41c7d0946804f2e98b05a31ea26a2d3b
home <fiegelshtein@gmail.com>
2017-08-28 22:27:36 +0300

    Add connect and disconnet event handlers

--- server/server.js
+++ server/server.js
@@ -1,13 +1,25 @@
 const path = require('path')
 const express = require('express')
+const socketIO = require('socket.io')
+const http = require('http')
 
-const publicPath = path.join(__dirname, '..',  'public')
+const publicPath = path.join(__dirname, '..', 'public')
 const port = process.env.PORT || 3000
 const app = express()
-
-//configure our app to use the public path(middleware)
+const server = http.createServer(app)
+const io = socketIO(server)
+// configure our app to use the public path(middleware)
 app.use(express.static(publicPath))
 
-app.listen(port, () => {
+io.on('connection', (socket) => {
+    console.log('New user connected')
+
+    socket.on('disconnect', (socket) => {
+        console.log('User was Disconnected')
+    })
+})
+
+
+server.listen(port, () => {
     console.log(`Started on post ${port}`)
 })
