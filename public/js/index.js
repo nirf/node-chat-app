bc41fbc5c59efaa042d5cff5a81ef9e642870602
home <fiegelshtein@gmail.com>
2017-09-01 18:04:35 +0300

    listen to events

---
+++ public/js/index.js
@@ -0,0 +1,9 @@
+let socket = io()
+
+socket.on('connect', function () {
+    console.log('connected to server')
+})
+
+socket.on('disconnect', function () {
+    console.log('Disconnected from server')
+})
